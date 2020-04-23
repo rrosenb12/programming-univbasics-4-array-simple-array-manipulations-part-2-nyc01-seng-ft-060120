e2344ff1107ba28ea1132a37e5bd5ad61e1f1152
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-23 15:36:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things = my_favorite_things.concat(more_favs)
 end
 
-def using_insert(array, element)
-  array.insert(4, "element")
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4, another_language)
 end
