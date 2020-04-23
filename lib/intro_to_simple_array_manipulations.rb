86bf12c34c727e4d610792abc5dbe1b16a86f9c8
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-23 15:14:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,5 +4,5 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   my_favorite_things.concat(more_favs)
-  binding.binding.pry
+  binding.pry
 end
